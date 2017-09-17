44125105eefba1f7793ea561c4f91bd856d6d0d3
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2017-09-17 19:08:10 +0200

    Added biopet/bootstrap theme

--- app/nl/biopet/report/controllers/Sample.scala
+++ app/nl/biopet/report/controllers/Sample.scala
@@ -5,12 +5,12 @@ import javax.inject.Inject
 import play.api.mvc.{AbstractController, ControllerComponents}
 
 class Sample @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
-  def singleName(projectId: Int, runId: Int, sample: String) = TODO // TODO
+  def singleName(projectId: Int, runId: Int, sampleName: String) = TODO // TODO
   def single(projectId: Int, runId: Int, sample: Int) = TODO // TODO
 
   def runSamples(projectId: Int, runId: Int) = TODO // TODO
 
-  def projectSamples(projectId: Int) = TODO
+  def projectSamples(projectId: Int) = TODO // TODO
 
   def allSamples = TODO // TODO
 
