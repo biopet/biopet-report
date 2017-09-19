1afd1dbc936f73c3ae104b67977667be15062963
pjvan_thof <pjrvanthof@gmail.com>
2017-09-19 13:17:49 +0200

    Reformat

--- app/nl/biopet/report/controllers/Project.scala
+++ app/nl/biopet/report/controllers/Project.scala
@@ -4,7 +4,8 @@ import javax.inject.Inject
 
 import play.api.mvc.{AbstractController, ControllerComponents}
 
-class Project @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+class Project @Inject()(cc: ControllerComponents)
+    extends AbstractController(cc) {
   def single(projectId: Int) = TODO //TODO
   def all = TODO //TODO
 
